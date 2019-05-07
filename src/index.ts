814ed0e3d8ec892b6f146fb7fab90681bf9fed2c
timbophillips <timothy.john.phillips@gmail.com>
2019-05-07 21:57:04 +0800

    typescript

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,7 @@
-import _ from 'lodash';
-
 function component() {
     const element = document.createElement('div');
   
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.innerHTML = "Hello typescript on webpack";
   
     return element;
   }
