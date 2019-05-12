2dff41cbd08b6d00a7c43ef1f8320fd6df6d84e3
timbophillips <timothy.john.phillips@gmail.com>
2019-05-12 20:24:02 +0800

    html-loader working

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
-// import "bootstrap";
-// import "bootstrap/dist/css/bootstrap.min.css";
-import '../src/style.css';
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import './style.css';
+
+import * as gridHTML from './grid.html';
+
+console.log(gridHTML);
 
 function component() {
   const element = document.createElement("div");
-  element.innerHTML = /*html*/ `
-<p> tim </p>
-  `;
+  // element.innerHTML = /*html*/ `<p class="lead"> ben </p>`;
+  element.innerHTML = gridHTML;
   return element;
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: "./src/index.ts",
+  // entry: "./src/index.ts",
   devtool: "inline-source-map",
   devServer: {
     contentBase: "./dist"
@@ -10,12 +10,22 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.js$/,
         use: "ts-loader",
         exclude: /node_modules/
       },
       {
         test: /\.css$/,
         use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.(html)$/,
+        use: {
+          loader: 'html-loader',
+          // options: {
+          //   attrs: [':data-src']
+          // }
+        }
       }
     ]
   },
@@ -24,8 +34,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: "timmy poo ",
-      template: '!!ejs-loader!src/index.html'
+      title: "Typescript Webpack Barebones",
     })
   ],
   output: {
