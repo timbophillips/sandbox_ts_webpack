0b728f7c9c37f0a25dcc35e0405f65058a0e38aa
timbophillips <timothy.john.phillips@gmail.com>
2019-05-17 21:12:23 +0800

    play continues

--- src/index.ts
+++ src/index.ts
@@ -18,29 +18,47 @@ const newDiv = addDivToDocument({
   document: document
 });
 
+
+let y = new doSomeStuff();
+
 // write testing 456 in all "target" elements
 addHtmlToDivsByClass({
   outerElement: newDiv,
   className: "output-box",
-  html: /*html*/ `<samp>ts code output goes here</samp>`
+  html: ""
 });
 
 // when button1 is clicked do something
 document.getElementsByName("button1").item(0).onclick = () => {
+  const box = document.getElementsByName("dbname")[0] as HTMLInputElement;
+  const dbname = box.value;
+  y.initDB(dbname);
   addHtmlToDivsByClass({
     outerElement: newDiv,
     className: "output-box",
-    html: /*html*/ `<samp>, and so on and so forth</samp>`
+    html: y.db.openingMsg + "<br/>"
   });
 };
 
 // when button2 is clicked do something else
 document.getElementsByName("button2").item(0).onclick = () => {
+  const box = document.getElementsByName("colname")[0] as HTMLInputElement;
+  const colname = box.value;
+  const z = y.createCollection<{ firstname: string; age: number }>(colname)
   addHtmlToDivsByClass({
     outerElement: newDiv,
     className: "output-box",
-    html: /*html*/ `<samp>, this could go forever</samp>`
+    html: z.openingMsg + "<br/>"
   });
 };
 
-doSomeStuff();
+// when button2 is clicked do something else
+document.getElementsByName("button3").item(0).onclick = () => {
+  let a: string = "";
+  y.collectionNamesInDb.map(x => (a += x + "<br/>"));
+  addHtmlToDivsByClass({
+    outerElement: newDiv,
+    className: "output-box",
+    html: a + "<br/>"
+  });
+};
--- src/ts/playground.ts
+++ src/ts/playground.ts
@@ -1,74 +1,69 @@
-export class Database {
-  private _online: boolean = true;
-  private collections: Array<Collection> = [];
-  constructor(public name: string) {
-    alert("new database created named " + name);
-  }
-  public toggleConnection = () => {
-    this._online = !this._online;
-  };
-  public get online() {
-    return this._online;
-  }
-  public addCollection(collection: Collection): string {
-    if (this.collections.find(x => x.name === collection.name)) {
-      return "already have a collection named " + collection.name;
-    } else {
-      this.collections.push(collection);
-      return (
-        "new collection added to me (" +
-        this.name +
-        ") and it is called ... " +
-        collection.name
-      );
-    }
-  }
-  public get collectionNames(): string {
-    let names: string = "";
-    this.collections.map(x => {
-      names += x.name + " ";
-    });
-    return names;
-  };
-}
-export class Collection {
-  openingMsg: string;
+export class Collection<T> {
+  public openingMsg: string;
   constructor(public db: Database, public name: string) {
-    this.openingMsg = db.addCollection(this);
+    this.openingMsg =
+      "i've been created and my name is " +
+      name +
+      ", i am a collection inside " +
+      db.name;
   }
   get feedback(): string {
     return this.openingMsg;
   }
+  get recycled(): Collection<any> {
+    this.openingMsg =
+      "i've been recycled and my name is " +
+      this.name +
+      ", i am a collection inside " +
+      this.db.name;
+    return this;
+  }
 }
 
-export function doSomeStuff() {
-  let db: Database;
-  let tim: Collection;
+export class Database {
+  private _collections: Array<Collection<any>> = [];
+  public openingMsg: string = "";
+  constructor(public name: string) {
+    this.openingMsg = "new database created named " + name;
+  }
+  public addNewOrExistingCollection<T>(name: string): Collection<T> {
+    const existingCollection = this._collections.find(x => x.name === name);
+    if (existingCollection) {
+      return existingCollection.recycled;
+    } else {
+      const newCollection = new Collection(this, name);
+      this._collections.push(newCollection);
+      return newCollection;
+    }
+  }
 
-  let button1 = document.createElement("button");
-  button1.textContent = "create database named phillips";
-  button1.onclick = () => {
-    db = new Database("Phillips");
-  };
+  public get collections(): Array<Collection<any>> {
+    return this._collections;
+  }
+}
 
-  let text1 = document.createElement("input");
+export class doSomeStuff {
+  db!: Database;
+  tim: any;
 
-  let button2 = document.createElement("button");
-  button2.textContent =
-    "create collection called tim in database named phillips";
-  button2.onclick = () => {
-    tim = new Collection(db, text1.value || "tim");
-    alert(tim.feedback);
-  };
+  constructor() {
+  }
 
-  let button3 = document.createElement("button");
-  button3.textContent = "list collections in database";
-  button3.onclick = () => {
-    alert(db.collectionNames);
-  };
+  public initDB(name: string): Database {
+    this.db = new Database(name);
+    return this.db;
+  }
 
-  document.body.appendChild(button1);
-  document.body.appendChild(button2);
-  document.body.appendChild(text1);
-  document.body.appendChild(button3);
+  public createCollection<T>(name: string): Collection<any> {
+    const z = this.db.addNewOrExistingCollection<T>(name);
+    return z;
+  }
+
+  public get collectionNamesInDb(): Array<string> {
+    let names: Array<string> = [];
+    this.db.collections.map(x => {
+      names.push(x.name);
+    });
+    return names;
+  }
 }
