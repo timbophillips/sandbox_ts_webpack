24c7197bb55a5971696fc7b8ccd21d320219b160
timbophillips <timothy.john.phillips@gmail.com>
2019-05-27 12:29:14 +0800

    r

--- src/ts/utils.ts
+++ src/ts/utils.ts
@@ -1,3 +1,5 @@
+import { } from 'rxjs';
+
 const addDivToDocument = ({
   html,
   document
@@ -36,4 +38,51 @@ const addHtmlToDivsByClass = ({
   return items;
 };
 
-export { addDivToDocument, addHtmlToDivsByClass };
+
+
+class boxForObs {
+
+  private _template = /*html*/`
+  <div class="col-12 docs-box pre-scrollable"></div>
+  `
+
+  private _style = /*css*/`
+  .output-box, .docs-box {
+    margin-left: 1rem;
+    margin-right: 1rem;
+    padding-top: 1rem;
+    padding-bottom: 1rem;
+    background-color: rgba(86, 61, 124, 0.15);
+    border: 1px solid rgba(86, 61, 124, 0.2);
+    min-height: 40rem;
+    max-height: 40rem;
+  }
+  `
+
+
+  constructor(private document: Document) {
+  }
+  
+  component = () => {
+    const element = document.createElement("div");
+    element.innerHTML = this._template;
+    element.setAttribute("style", this._style);
+    return element
+  }
+}
+
+customElements.define('box-observable',
+  class extends HTMLElement {
+    constructor() {
+      super();
+
+      const pElem = document.createElement('p');
+      pElem.textContent = this.getAttribute('observable');
+
+      const shadowRoot = this.attachShadow({mode: 'open'});
+      shadowRoot.appendChild(pElem);
+    }
+  }
+);
+
+export { addDivToDocument, addHtmlToDivsByClass, boxForObs };
